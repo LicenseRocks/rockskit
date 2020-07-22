cd68807a8bfae64738d979b1f3d7d10cd5062f6e
Majid Amiri <majid@licenserocks.de>
2020-07-22 15:45:46 +0430

    Fix story container

--- .storybook/decorators/container.js
+++ .storybook/decorators/container.js
@@ -1,21 +1,16 @@
 import React from "react";
-import styled from "styled-components";
 import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "styled-components";
 
 import { theme } from "../../src/theme";
 
-const StyledWrapper = styled.div`
-  position: relative;
-`;
-
 export const Container = (story) => {
   return (
     <MuiThemeProvider theme={theme}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <StyledWrapper>{story()}</StyledWrapper>
+        {story()}
       </ThemeProvider>
     </MuiThemeProvider>
   );
