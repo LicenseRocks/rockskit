5c4b44874c6fa1a154bad9b21775dd68cd4e9cdc
Majid <majid@licenserocks.de>
2020-12-28 19:49:20 +0330

    add popover to mini share module

--- src/MiniShareModule/MiniShareModule.js
+++ src/MiniShareModule/MiniShareModule.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import styled, { css } from "styled-components";
+import Popover from "@material-ui/core/Popover";
 
 import { ShareModulePropTypes, ShareModuleDefaultProps } from ".";
-import { Icon } from "../Icon";
+import { Button, Icon } from "..";
 import { DISPLAY, SPACER } from "../theme";
+
+const BUTTON_SIZE = 32;
+
+const StyledPopover = styled(Popover)`
+  .MuiPopover-paper {
+    border-radius: 8px;
+    background-color: ${({ theme }) => theme.palette.common.white};
+    box-shadow: 0px 16px 56px rgba(41, 40, 57, 0.16);
+    padding: ${({ theme }) => theme.spacing(2)};
+    margin-top: ${BUTTON_SIZE + 8}px;
+  }
+`;
+
+const StyledButton = styled(Button)`
+  && {
+    min-width: ${BUTTON_SIZE}px;
+    max-width: ${BUTTON_SIZE}px;
+    height: ${BUTTON_SIZE}px;
+  }
+
+  ${(theme) => SPACER(theme)}
+  ${(theme) => DISPLAY(theme)}
+`;
 
 const Container = styled.div`
   display: inline-flex;
@@ -38,13 +62,25 @@ const Container = styled.div`
       }}
     }
   }
-
-  ${(theme) => SPACER(theme)}
-  ${(theme) => DISPLAY(theme)}
 `;
 
-export const MiniShareModule = ({ iconProps, shareOptions, url, ...props }) => {
+export const MiniShareModule = ({
+  iconProps,
+  orientation,
+  shareOptions,
+  url,
+  ...props
+}) => {
+  const [anchorEl, setAnchorEl] = useState(null);
   const encodedUrl = encodeURIComponent(url);
+
+  const handlePopoverOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
 
   const renderIcon = (icon, href) => (
     <a href={href} rel="noreferrer" target="_blank">
@@ -53,25 +89,41 @@ export const MiniShareModule = ({ iconProps, shareOptions, url, ...props }) => {
   );
 
   return (
-    <Container {...props}>
-      {shareOptions.includes("twitter") &&
-        renderIcon(
-          "twitter",
-          `https://twitter.com/intent/tweet?url=${encodedUrl}`
-        )}
+    <>
+      <StyledButton
+        icon="share-alt"
+        onClick={handlePopoverOpen}
+        size="sm"
+        {...props}
+      />
 
-      {shareOptions.includes("facebook") &&
-        renderIcon(
-          "facebook-f",
-          `https://www.facebook.com/sharer.php?u=${encodedUrl}`
-        )}
+      <StyledPopover
+        id="share-module-popover"
+        open={!!anchorEl}
+        anchorEl={anchorEl}
+        onClose={handlePopoverClose}
+      >
+        <Container orientation={orientation}>
+          {shareOptions.includes("twitter") &&
+            renderIcon(
+              "twitter",
+              `https://twitter.com/intent/tweet?url=${encodedUrl}`
+            )}
 
-      {shareOptions.includes("linkedin") &&
-        renderIcon(
-          "linkedin-in",
-          `https://www.linkedin.com/sharing/share-offsite/?url=${encodedUrl}`
-        )}
-    </Container>
+          {shareOptions.includes("facebook") &&
+            renderIcon(
+              "facebook-f",
+              `https://www.facebook.com/sharer.php?u=${encodedUrl}`
+            )}
+
+          {shareOptions.includes("linkedin") &&
+            renderIcon(
+              "linkedin-in",
+              `https://www.linkedin.com/sharing/share-offsite/?url=${encodedUrl}`
+            )}
+        </Container>
+      </StyledPopover>
+    </>
   );
 };
 
