b40bbfce120a0c24b1ba4d8742fe2c893c7b8346
Kacper Hernacki <65904012+Kacper-Hernacki@users.noreply.github.com>
2021-10-15 20:20:45 +0200

    stories configs for crrencies db

--- src/Form/Price/stories.js
+++ src/Form/Price/stories.js
@@ -27,6 +27,12 @@ export const main = () => {
     },
     register,
     startIcon: "money-bill",
+    currenciesFromDB: [
+      { sign: "€", label: "EUR (€)", value: "eur" },
+      { sign: "zł", label: "PLN (zł)", value: "pln" },
+      { sign: "$", label: "USD ($)", value: "usd" },
+      { sign: "£", label: "GBP (£)", value: "gbp" },
+    ],
   };
 
   const values = watch();
