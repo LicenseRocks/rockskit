1c734762216e10499b68c478cdcae24ac0cd5cbd
Igor Klepacki <neg4n@icloud.com>
2022-08-03 14:12:47 +0200

    feat: redesign dropdown api and logic

--- src/Dropdown/Dropdown.js
+++ src/Dropdown/Dropdown.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useRef } from "react";
+import { omit } from "lodash";
 import styled, { useTheme } from "styled-components";
 import Dialog from "@material-ui/core/Dialog";
 import Menu from "@material-ui/core/Menu";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
 import MenuItem from "@material-ui/core/MenuItem";
 
 import { useMediaQuery } from "..";
@@ -14,7 +16,6 @@ const StyledDropdown = styled(Menu)`
   }
 
   .MuiMenu-paper {
-    position: relative;
     border-radius: 12px;
     pointer-events: all;
     background-color: ${({ theme }) => theme.palette.common.white};
@@ -39,26 +40,124 @@ const StyledDialog = styled(Dialog).attrs(() => ({
   }
 `;
 
-export const Dropdown = ({ children, items, responsive, ...props }) => {
+function useOpen() {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const close = () => {
+    setIsOpen(false);
+  };
+
+  const open = () => {
+    setIsOpen(true);
+  };
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return {
+    close,
+    open,
+    toggle,
+    isOpen,
+  };
+}
+
+const useMousePosition = () => {
+  const [mousePosition, setMousePosition] = React.useState({
+    x: null,
+    y: null,
+  });
+  React.useEffect(() => {
+    const updateMousePosition = (ev) => {
+      setMousePosition({ x: ev.clientX, y: ev.clientY });
+    };
+    window.addEventListener("mousemove", updateMousePosition);
+    return () => {
+      window.removeEventListener("mousemove", updateMousePosition);
+    };
+  }, []);
+  return mousePosition;
+};
+
+export const Dropdown = ({
+  children,
+  items,
+  disableOutsideClickClose,
+  responsive,
+  render,
+  ...props
+}) => {
   const theme = useTheme();
+  const { close, open, toggle, isOpen } = useOpen();
+  const ref = React.useRef();
+  const { x, y } = useMousePosition();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const sanitizedRestProps = omit(props, "open");
 
   if (responsive && isMobile)
     return (
-      <StyledDialog fullScreen hideBackdrop {...props}>
-        {children}
-      </StyledDialog>
+      <>
+        {React.cloneElement(children, {
+          onClick: () => {
+            toggle();
+          },
+          ref,
+          ...children.props,
+        })}
+        <StyledDialog
+          fullScreen
+          hideBackdrop
+          open={isOpen}
+          {...sanitizedRestProps}
+        >
+          {render({ close, toggle, open, isMobile, isOpen })}
+        </StyledDialog>
+      </>
     );
 
   return (
-    <StyledDropdown {...props}>
-      {children ||
-        items.map(({ label, onClick, value, ...itemProps }) => (
-          <StyledItem key={label} onClick={() => onClick(value)} {...itemProps}>
-            {label}
-          </StyledItem>
-        ))}
-    </StyledDropdown>
+    <>
+      {React.cloneElement(children, {
+        onClick: () => {
+          toggle();
+        },
+        ref,
+        ...children.props,
+      })}
+
+      <ClickAwayListener
+        mouseEvent="onMouseDown"
+        onClickAway={() => {
+          if (!disableOutsideClickClose) {
+            if (
+              document.elementsFromPoint(x, y).reverse().pop() === ref.current
+            ) {
+              return;
+            }
+
+            if (isOpen) {
+              close();
+            }
+          }
+        }}
+      >
+        <StyledDropdown open={isOpen} {...sanitizedRestProps}>
+          {render
+            ? render({ close, toggle, open, isMobile, isOpen })
+            : items.map(({ label, onClick, value, ...itemProps }) => (
+                <StyledItem
+                  key={label}
+                  onClick={() => onClick(value)}
+                  {...itemProps}
+                >
+                  {label}
+                </StyledItem>
+              ))}
+        </StyledDropdown>
+      </ClickAwayListener>
+    </>
   );
 };
 
