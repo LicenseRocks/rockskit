e406fed9f93e0645a38d3937a70f6f32f5f8b57f
Igor Klepacki <neg4n@icloud.com>
2022-09-20 08:49:15 +0200

    fix: icons error and add new stories to icon

--- src/Icon/stories.js
+++ src/Icon/stories.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { select, boolean } from "@storybook/addon-knobs";
+import { select, text, boolean } from "@storybook/addon-knobs";
 
 import { StoryWrapper } from "../../.storybook/decorators";
 import { KIT_ICON_SIZES, THEME_COLORS } from "../theme";
@@ -13,10 +13,12 @@ export default {
 export const main = () => {
   const defaultProps = {
     color: select("Color", THEME_COLORS, "primary"),
+    prefix: select("Prefix", ["fa", "far"], "far"),
+    icon: text("Icon name", "hello"),
     rounded: boolean("Rounded", false),
     squared: boolean("Squared", false),
     size: select("Size", Object.keys(KIT_ICON_SIZES), "md"),
   };
 
-  return <Icon icon="check-circle" {...defaultProps} />;
+  return <Icon {...defaultProps} />;
 };
